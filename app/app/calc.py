d8dbc630bebf71936a64bb49e66d156a8fb6c458
elprincechidi <princechidie@gmail.com>
2026-04-02 16:21:27 +0100

    migrations

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,10 +1,9 @@
 """calculator functions"""
 
+
 def add(x, y):
     return x + y
 
+
 def subtract(x, y):
     return x - y
-
-def test_get_greeting():
-    return {'greeting': 'Hello, World'}
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,31 +1,14 @@
 """sample tests"""
-
 from django.test import SimpleTestCase
 from app.calc import add
 from app.calc import subtract
-from app.calc import test_get_greeting
-from rest_framework.test import APIClient
 
 
 class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
-        
         res = add(3, 4)
         self.assertEqual(res, 7)
-        
-    
-    
+
     def test_subtract_numbers(self):
         res = subtract(10, 5)
         self.assertEqual(res, 5)
-        
-        
-class TestView(SimpleTestCase):
-    """test getting api view"""
-    def test_get_greeting(self):
-        client = APIClient()
-        res = client.get('/greeting/')
-    
-        
-        self.assertEqual(res.data, {'greeting': 'Hello, World'},)
-        
